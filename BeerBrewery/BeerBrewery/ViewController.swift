fb5063a898c30ae7ac5154af0aa58b08eedce484
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2022-02-04 17:07:45 -0300

    Add viewController initial layout

--- BeerBrewery/BeerBrewery/ViewController.swift
+++ BeerBrewery/BeerBrewery/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldSearch: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageTextfield()
     }
-
-
+    
+    func imageTextfield() {
+        let imageview = UIImageView(frame: CGRect(x: 16, y: 8, width: 23, height: 23))
+        let image = UIImage(named: "iconSearch")
+        imageview.image = image
+        imageview.contentMode = .scaleAspectFit
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        view.addSubview(imageview)
+        textFieldSearch?.leftViewMode = .always
+        textFieldSearch?.leftView = view
+    }
 }
 
