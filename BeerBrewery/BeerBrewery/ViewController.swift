c16bdab4bcbe8b7ae3cafe7009916457175b7f9b
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2022-02-04 09:18:40 -0300

    Initial configuration

---
+++ BeerBrewery/BeerBrewery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerBrewery
+//
+//  Created by Paulo Henrique Bendazzoli on 04/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BeerBrewery/Pods/Target Support Files/Pods-BeerBrewery-BeerBreweryUITests/Pods-BeerBrewery-BeerBreweryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BeerBrewery_BeerBreweryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BeerBrewery_BeerBreweryUITests
+@end
